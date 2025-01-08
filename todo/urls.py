0fd71232a66674d712d6728542fb669854b08239
Charlton-C <mileswill32@protonmail.com>
2025-01-09 01:17:06 +0300

    Fix form not submitting and not handling response correctly

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),  # Show todo list page
-    path('add/', views.add_todo, name='add_todo'),  # Add new todo item
+    path('add', views.add_todo, name='add_todo'),  # Add new todo item
 ]
