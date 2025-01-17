178c18a9049d4052db7cf12a8a27164b9ab39518
Charlton-C <mileswill32@protonmail.com>
2025-01-17 20:09:46 +0300

    Reformat files to use spaces instead of tabs

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class TodoItem(models.Model):
-	text = models.CharField(max_length=255)
-	completed = models.BooleanField(default=False)
+    text = models.CharField(max_length=255)
+    completed = models.BooleanField(default=False)
 
-	def __str__(self):
-		return self.text
+    def __str__(self):
+        return self.text
--- todo/urls.py
+++ todo/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.todo_list, name='todo_list'),
-	path('add', views.add_todo, name='add_todo'),
-	path('update', views.update_todo, name='update_todo'),
-	path('delete', views.delete_todo, name='delete_todo'),
+    path('', views.todo_list, name='todo_list'),
+    path('add', views.add_todo, name='add_todo'),
+    path('update', views.update_todo, name='update_todo'),
+    path('delete', views.delete_todo, name='delete_todo'),
 ]
