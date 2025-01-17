c0abd3e520a9d49aae237542f8dd58e3a0143ce4
Charlton-C <mileswill32@protonmail.com>
2025-01-17 20:45:17 +0300

    Add error handling to views

--- todo/views.py
+++ todo/views.py
@@ -1,64 +1,69 @@
 from django.http import JsonResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from .models import TodoItem
 
 
 def todo_list(request):
-    todos = TodoItem.objects.all()
-    return render(request, 'todo_list.html', {'todos': todos})
+    try:
+        todos = TodoItem.objects.all()
+        return render(request, 'todo_list.html', {'todos': todos})
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': 'An Error Occurred'}, status=500)
 
 def add_todo(request):
-    if request.method == 'POST':
-        text = request.POST.get('new_todo')
-        if text:
-            new_todo = TodoItem.objects.create(text=text)
-            return JsonResponse({
-                'success': True,
-                'id': new_todo.id,
-                'text': new_todo.text,
-                'completed': new_todo.completed,
-            }, status=200)
+    try:
+        if request.method == 'POST':
+            text = request.POST.get('new_todo')
+            if text:
+                new_todo = TodoItem.objects.create(text=text)
+                return JsonResponse({
+                    'success': True,
+                    'id': new_todo.id,
+                    'text': new_todo.text,
+                    'completed': new_todo.completed,}, status=200)
+            else:
+                return JsonResponse({'success': False, 'error': 'Todo text is required'}, status=400)
         else:
-            return JsonResponse({'error': 'Todo text is required'}, status=400)
-    return JsonResponse({'error': 'Invalid request method'}, status=405)
+            return JsonResponse({'success': False, 'error': 'Invalid request method'}, status=405)
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': 'An Error Occurred'}, status=500)
 
 def update_todo(request):
     try:
         if request.method == 'POST':
-            id = request.POST.get('id')
-            if id:
-                todo = TodoItem.objects.get(id=id)
-                if todo:
-                    todo.completed = not todo.completed
-                    todo.save()
-                    return JsonResponse({
-                        'success': True,
-                        'new_todo_completed': todo.completed
-                    }, status=200)
-                else:
-                    return JsonResponse({
-                        'success': False,
-                        'error': 'Todo not found'
-                    }, status=400)
-            else:
-                return JsonResponse({'error': 'Todo id is required'}, status=400)
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
+            todo_id = request.POST.get('id')
+            if not todo_id:
+                return JsonResponse({'success': False, 'error': 'Todo id is required'}, status=400)
+
+            try:
+                todo = TodoItem.objects.get(id=todo_id)
+            except TodoItem.DoesNotExist:
+                return JsonResponse({'success': False, 'error': 'Todo not found'}, status=400)
+
+            todo.completed = not todo.completed
+            todo.save()
+
+            return JsonResponse({'success': True, 'todo_completed': todo.completed}, status=200)
+        else:
+            return JsonResponse({'success': False, 'error': 'Invalid request method'}, status=405)
     except Exception as e:
-        return JsonResponse({'error': 'An Internal Server Error Occurred'}, status=500)
+        return JsonResponse({'success': False, 'error': 'An Error Occurred'}, status=500)
 
 def delete_todo(request):
     try:
         if request.method == 'POST':
-            id = request.POST.get('id')
-            if id:
-                try:
-                    todo = TodoItem.objects.get(id=id)
-                    todo.delete()
-                    return JsonResponse({'success': True})
-                except Todo.DoesNotExist:
-                    return JsonResponse({'success': False, 'error': 'Todo not found'}, status=400)
-            else:
+            todo_id = request.POST.get('id')
+            if not todo_id:
                 return JsonResponse({'success': False, 'error': 'Todo id is required'}, status=400)
-        return JsonResponse({'success': False, 'error': 'Invalid request method'}, status=405)
+
+            try:
+                todo = TodoItem.objects.get(id=todo_id)
+            except TodoItem.DoesNotExist:
+                return JsonResponse({'success': False, 'error': 'Todo not found'}, status=400)
+
+            todo.delete()
+            return JsonResponse({'success': True}, status=200)
+        else:
+            return JsonResponse({'success': False, 'error': 'Invalid request method'}, status=405)
     except Exception as e:
-        return JsonResponse({'success': False, 'error': 'An Internal Server Error Occurred'}, status=500)
+        return JsonResponse({'success': False, 'error': 'An Error Occurred'}, status=500)
